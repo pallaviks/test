31a2149a9736c286b78a4a41cb2eb345db22261e
pallaviks <pallaviks.bhat@gmail.com>
2026-02-28 12:13:13 +0530

    Create src/App.js that imports and uses CalculatorAgent component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalculatorAgent from './CalculatorAgent';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Calculator App</h1>
+            <CalculatorAgent />
+        </div>
+    );
+};
+
+export default App;
